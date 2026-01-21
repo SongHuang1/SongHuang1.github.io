7652173dfee4dd844e4410ea9733268fd815a315
SongHuang <138837991+SongHuang1@users.noreply.github.com>
2026-01-21 19:38:28 +0800

    最后一次大修

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'eexperience', 'blog']
+const section_names = ['home', 'experience', 'blog']
 
 
 window.addEventListener('DOMContentLoaded', event => {
